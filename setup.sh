dc0150dd3823b7ddb2318da2553d5e77730069fb
Mac <contact@maclong.dev>
2026-08-08 16:26:02 +0100

    Install shared skills from local checkout

--- setup.sh
+++ setup.sh
@@ -39,8 +39,8 @@ fi
 
 "$mise" -C "$dotfiles" trust
 "$mise" -C "$dotfiles" install
-"$mise" -C "$dotfiles" run skills
 "$mise" -C "$dotfiles" exec -- mise bootstrap --yes --force-dotfiles
+"$mise" -C "$dotfiles" run skills
 
 if [ "$(uname -s)" = "Darwin" ]; then
   sudo_file=/etc/pam.d/sudo_local
